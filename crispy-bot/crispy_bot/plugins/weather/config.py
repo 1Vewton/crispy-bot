b8ef82ce23cc0b41728771d6d8fdc6b92880c46a
NewtonZhan <zhanyunze0601@gmail.com>
2026-04-17 13:34:52 +0800

    feat(config): Add plugin cofig for weather

---
+++ crispy-bot/crispy_bot/plugins/weather/config.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+
+class Config(BaseModel):
+    """Plugin Config Here"""
+    llm_base_url: str = ""
+    llm_api_key: str = ""
+    llm_provider: str = ""
+    llm_model_name: str = ""
+    private_key_qweather_path: str = ""
