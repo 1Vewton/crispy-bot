46fc9c1cb84c5c4ed96ddc7e43c5984762c7ecf2
NewtonZhan <zhanyunze0601@gmail.com>
2026-04-17 14:51:16 +0800

    feat(weather): Add weather private key test command

--- crispy-bot/crispy_bot/plugins/weather/__init__.py
+++ crispy-bot/crispy_bot/plugins/weather/__init__.py
@@ -1,4 +1,4 @@
-from nonebot import get_plugin_config
+from nonebot import get_plugin_config, on_command
 from nonebot.plugin import PluginMetadata
 
 from .config import Config
@@ -12,4 +12,25 @@ __plugin_meta__ = PluginMetadata(
 
 # Config
 config = get_plugin_config(Config)
+private_key_ready = False
+try:
+    with open(config.private_key_qweather_path, "r") as f:
+        private_key_ready = True
+        private_key = f.read()
+except FileNotFoundError:
+    private_key = "No key exists."
+
+# Commands
+weather_test = on_command(
+    "testWeatherCommand"
+)
+
+
+# Test whether the private key presents
+@weather_test.handle()
+async def weather_test_process():
+    result = "私钥已被读取，允许进行天气信息获取"
+    if not private_key_ready:
+        result = "私钥读取失败，天气信息调取无法成功"
+    await weather_test.finish(result)
 
