746237952636149dfd2ca9aace58e018b2d2d316
NewtonZhan <zhanyunze0601@gmail.com>
2026-04-30 11:21:32 +0800

    fix(adaptors): Remove terminal adaptor

--- crispy-bot/bot.py
+++ crispy-bot/bot.py
@@ -4,8 +4,6 @@ from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 
 from nonebot.adapters.onebot.v12 import Adapter as ONEBOT_V12Adapter
 
-from nonebot.adapters.console import Adapter as CONSOLEAdapter
-
 
 
 nonebot.init()
@@ -14,9 +12,6 @@ driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
 
 driver.register_adapter(ONEBOT_V12Adapter)
-
-driver.register_adapter(CONSOLEAdapter)
-
 nonebot.load_builtin_plugins('echo')
 
 
